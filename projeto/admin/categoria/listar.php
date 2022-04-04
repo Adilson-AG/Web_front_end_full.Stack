b200c64c78668c4e57f6ed0c44dcb76e93e2870c
Adilson Antonio Genari Filho <80858375+Adilson-AG@users.noreply.github.com>
2022-04-04 13:18:58 -0300

    Add files via upload

---
+++ projeto/admin/categoria/listar.php
@@ -0,0 +1,37 @@
+<h3 class="text-primary" 
+>Listar categoria
+<a href="?p=categoria/salvar" class="btn btn-success float-right">
+	Adicionar
+
+</a>
+
+</h3>
+<div class="card shadow mt-4">
+	
+
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Cod.</th>
+      <th scope="col">Descrição</th>
+      <th scope="col">Opções</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Plástico</td>
+      <td>
+      	<a type ="button" class="btn btn-success" style ="color: #fff"><i class="bi bi-pencil-fill"></i></a>
+      	<a type ="button" class="btn btn-danger" data-confirm=" Excluir registro?"><i class="bi bi-trash3"></i></i></a>
+      
+      </td>
+      
+    </tr>
+   
+  </tbody>
+</table>
+</div>
+
